c476d15ff09d26981b5fa1bcd0f00e11e56bec00
HeathRL <hrleek@gmail.com>
2019-04-11 18:36:15 -0500

    added sql file to repo

---
+++ ETL Project DB.sql
@@ -0,0 +1,38 @@
+DROP DATABASE Restaurant_db;
+CREATE DATABASE Restaurant_db;
+
+USE Restaurant_db;
+
+CREATE TABLE inspection(
+restaurant_id VARCHAR(9) NOT NULL,
+restaurant_name VARCHAR(255),
+restaurant_address VARCHAR(255),
+restaurant_city VARCHAR(45),
+restaurant_state VARCHAR(2),
+restaurant_zip INT(5),
+health_inspection_score INT(2),
+health_inspection_grade VARCHAR(1),
+PRIMARY KEY(restaurant_id)
+);
+
+CREATE TABLE yelp_restaurant_data(
+yelp_id VARCHAR(255) NOT NULL,
+yelp_rest_name VARCHAR(255),
+yelp_rating FLOAT(3, 2),
+yelp_review_count INT(6),
+yelp_rest_address VARCHAR(255) NOT NULL,
+yelp_rest_city VARCHAR(45),
+yelp_rest_state VARCHAR(2),
+yelp_rest_zip INT(5),
+PRIMARY KEY (yelp_id),
+FOREIGN KEY (yelp_rest_address) REFERENCES inspection(restaurant_address)
+);
+
+CREATE TABLE yelp_review_data(
+yelp_id VARCHAR(255) NOT NULL,
+yelp_rest_name VARCHAR(255),
+yelp_rating FLOAT(3, 2),
+yelp_review_text VARCHAR(255),
+PRIMARY KEY (yelp_id)
+);
+
